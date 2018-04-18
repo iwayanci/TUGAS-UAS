21648a59358c71cd61ac9900d833b041831496bb
i wayan m cipta i <37320820+iwayanci@users.noreply.github.com>
2018-04-18 14:58:06 +0700

    Add files via upload

---
+++ UAS.py
@@ -0,0 +1,18 @@
+import iwayanci.ifelse
+import iwayanci.random
+import iwayanci.append
+import iwayanci.tupple
+import iwayanci.dict
+import iwayanci.opp
+
+print (iwayanci.ifelse.fa())
+
+print (iwayanci.random.fb())
+
+print (iwayanci.append.fc())
+
+print (iwayanci.tupple.fd())
+
+print (iwayanci.dict.fe())
+
+print (iwayanci.opp.ff())
